a1eecbb3e4387ed25a0fea7bf2e2961a4e2a9e04
Patcharapol Sirimaneechot <61160017@go.buu.ac.th>
2021-02-18 17:59:45 +0700

    commit2

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const { MongoClient } = require('mongodb')
+const MongoClient = require('mongodb').MongoClient
 
 const url = 'mongodb+srv://61160017:123456@cluster0.qedpt.mongodb.net/sample_weatherdata?retryWrites=true&w=majority'
-const client = MongoClient(url)
+const client = new MongoClient(url)
+
+function run() {
+    try {
+
+        await client.connect()
+
+        const db = client.db('sample_weatherdata')
+
+        const collection = db.collection('data')
+
+        const query = { type: "FM-13" }
+        const air = await collection.findOne(query)
+        // console.log(air)
+        await cursor.forEach(console.dir)
+
+    } catch(e) {
+        console.log(e)
+    } finally {
+        await client.close()
+    }
+}
